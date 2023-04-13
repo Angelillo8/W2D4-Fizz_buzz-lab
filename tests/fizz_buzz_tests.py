f4adf30e88312b3126c090d103fb34989a40e531
Angel <angelgonzc@yahoo.com>
2023-04-13 11:09:55 +0100

    added minor fixes

--- tests/fizz_buzz_tests.py
+++ tests/fizz_buzz_tests.py
@@ -4,7 +4,6 @@ from src.fizz_buzz import *
 
 class TestFizzBuzz(unittest.TestCase):
     def setUp(self):
-        self.number = 0
         self.dict = {
             3:"Fizz",
             5:"Buzz",
@@ -14,23 +13,23 @@ class TestFizzBuzz(unittest.TestCase):
 
     def test_fizz_buzz(self):
         self.number = 15
-        result = fizz_buzz(self.dict,self.number)
+        result = fizz_buzz(self.dict,15)
         self.assertEqual("FizzBuzzRuzz",result)
     def test_fizz_buzz1(self):
         self.number = 50
-        result = fizz_buzz(self.dict,self.number)
+        result = fizz_buzz(self.dict,50)
         self.assertEqual("Buzz",result)
     def test_fizz_buzz2(self):
         self.number = 27
-        result = fizz_buzz(self.dict,self.number)
+        result = fizz_buzz(self.dict, 27)
         self.assertEqual("Fizz",result)
     def test_fizz_buzz3(self):
         self.number = 4
-        result = fizz_buzz(self.dict,self.number)
+        result = fizz_buzz(self.dict,4)
         self.assertEqual("4",result)
     def test_fizz_buzz4(self):
         self.number = 45
-        result = fizz_buzz(self.dict,self.number)
+        result = fizz_buzz(self.dict,45)
         self.assertEqual("FizzBuzzRuzz",result)
     # def test_fizz_buzz5(self):
     #     self.number = 82
